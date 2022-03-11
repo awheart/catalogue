2690f800ecd7df6efb7f8ea9823cd5efd108a1bb
Awheart <arthurvandelanoitte@live.fr>
2022-03-11 18:17:02 +0100

    starting page front

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,6 +15,24 @@
           <nuxt-link class="nav-link" to="/account"> My Account </nuxt-link>
         </li>
       </ul>
+      <ul class="navbar-nav"
+          v-if="!$auth.loggedIn">
+          <li class="nav-item">
+            <nuxt-link to="/user/register" class="nav-link" exact-active-class="active">Register</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/user/login" class="nav-link" exact-active-class="active">Login</nuxt-link>
+          </li>
+      </ul>
+      <ul class="navbar-nav"
+          v-if="$auth.loggedIn">
+          <li class="nav-item">
+            <nuxt-link to="/user/my-account" class="nav-link" exact-active-class="active">My Account</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link to="/user/logout" class="nav-link" exact-active-class="active">Logout</nuxt-link>
+          </li>
+        </ul>
     </nav>
     <nuxt />
   </div>
---
+++ pages/user/logout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Logging out</h1>
+    <hr>
+
+    <p>Please wait...</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'logoutPage',
+    middleware: 'auth',
+
+    async asyncData(context){
+        await context.$auth.logout()
+  }
+}
+</script>
