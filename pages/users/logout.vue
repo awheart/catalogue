c16a94f0f235d7a60db11e0117bb47778e459d34
Awheart <arthurvandelanoitte@live.fr>
2022-03-29 00:19:12 +0200

    logout page

---
+++ pages/users/logout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>You are now logged out !</h1>
+    <hr>
+
+    <p>Sending you back to home page, please wait !</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'logoutPage',
+  middleware: 'auth',
+
+  async asyncData(context) {
+    await context.$auth.logout()
+  }
+}
+</script>
