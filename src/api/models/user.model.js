9fd5d1b474b24bddb895ccd0787626ef1c70f7a1
Awheart <arthurvandelanoitte@live.fr>
2022-04-03 23:36:28 +0200

    user model update

--- src/api/models/user.model.js
+++ src/api/models/user.model.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const User = new Schema({
-    name: { type: String, required: true},
-    email: { type: String, required: true, index: { unique: true } },
+    username: { type: String, required: true},
+    email: { type: String, required: true,  unique: true },
     password: { type: String, required: true },
     role: { type: String, required: true, default: 'user' }
 },
@@ -11,6 +11,6 @@ const User = new Schema({
     collection: 'users',
     timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
 }
-)  
+)
 
 module.exports = mongoose.model('User', User)
