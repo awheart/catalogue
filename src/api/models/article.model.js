4a0399dac6add201855045a1d1816eb17894f11c
Awheart <arthurvandelanoitte@live.fr>
2022-04-03 23:34:45 +0200

    article model update

--- src/api/models/article.model.js
+++ src/api/models/article.model.js
@@ -3,13 +3,24 @@ const Schema = mongoose.Schema
 
 const Article = new Schema({
     title: { type: String, required: true, index: { unique: true } },
-    author: { type: String, required: true },
-    content: { type: String, required: true }
+    creator: { type: String, required: true },
+    posted_by: { type: Schema.Types.ObjectId, ref: 'users', required: true },
+    date_of_creation: { type: Date, required: true },
+    description: { type: String, required: true },
+    comments: [{
+            body: {
+                type: String, required: true
+            },
+            by: {
+                type: Schema.Types.ObjectId, ref: 'users'
+            }
+        }
+    ]
 },
-{ 
-    collection: 'articles',
-    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
-}
+    {
+        collection: 'articles',
+        timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
+    }
 )
 
 module.exports = mongoose.model('Article', Article)
