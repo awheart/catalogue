97d7c1ffb24fb5b4f6e91f66c4d1319b4c944638
Awheart <arthurvandelanoitte@live.fr>
2022-04-03 23:33:36 +0200

    handle errors + update

---
+++ src/api/utils/errors_handler.js
@@ -0,0 +1,18 @@
+exports.Error_Messages = Object.freeze({
+    user_id_is_invalid: 'L\'id de cet utilisateur est invalide.',
+    article_id_is_invalid: 'L\'id de cette email est invalide.',
+    user_is_empty: 'L\'utilisateur doit être renseigné.',
+    email_is_empty: 'L\'email doit être renseignée.',
+    title_is_empty: 'Le titre doit être renseigné.',
+    description_is_empty: 'Veuillez ajouter une description.',
+    user_not_found: 'Utilisateur introuvable',
+    article_not_found: 'Article introuvable.',
+    email_existing: 'Email déjà existante.',
+    title_existing: 'Titre déjà existant.',
+    user_existing: 'Utilisateur déjà existant',
+    password_is_empty: 'Le mot de passe doit faire doit contenir au moins 4 caractères.',
+    invalid_credentials: 'L\'email ou le mot de passe est incorrecte.',
+    error_saving: 'Une erreur est survenue durant la sauvegarde.',
+    incorrect_params: 'Paramètres incorrectes',
+    unauthorized_action: 'Vous n\'avez pas les permissions requises pour faire ceci.'
+})
