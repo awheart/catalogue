f9f4e30f685b1fcc3d06676fb08e4d67fa836b8a
Awheart <arthurvandelanoitte@live.fr>
2022-04-05 16:11:47 +0200

    Revert "config fix for deploy + env variables"
    This reverts commit 247cbb04fa5c6e6d42d822465526f94d868162da.

--- src/api/config.js
+++ src/api/config.js
@@ -21,7 +21,7 @@ module.exports.isAuthenticated = async(req, res, next) => {
 
 module.exports = {
     database: {
-        uri: process.env.MONGODB_URI,
+        uri: 'mongodb://localhost:27017/catalogue',
         options: { useUnifiedTopology: true }
     },
     server: { port: process.env.PORT || 8000 },
