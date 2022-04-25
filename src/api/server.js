4dc2c24de15c1127af07b21208979499482e8e32
Awheart <arthurvandelanoitte@live.fr>
2022-04-25 22:38:43 +0200

    init cors

--- src/api/server.js
+++ src/api/server.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const routes = require('./routes')
 const router = express.Router({ strict: true })
-const db = require('./db')
+const cors = require('cors')
 
 const api = express()
 
+api.use(cors())
 api.use(express.json())
 api.use(express.urlencoded({ extended: true }))
 api.use(router)
 api.use(routes)
-
-db.init()
 
 module.exports = api
