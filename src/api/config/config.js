a9c396314323b5f1ad57242451185f84a43c66c7
Awheart <arthurvandelanoitte@live.fr>
2022-04-25 22:35:02 +0200

    fix import routes

---
+++ src/api/config/config.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+
+const configSecret = {
+    authSecret: 'secret' // secret to generate jwt token
+}
+
+// check if user is connecter 
+module.exports.isAuthenticated = async (req, res, next) => {
+    var token = req.headers.authorization
+    if (token) {
+        // check if token is valid and verifies secret
+        jwt.verify(token.replace(/^Bearer\s/, ''), configSecret.authSecret, (err) => {
+            return (err ? res.status(401).json({ message: 'Unauthorized' }) : next())
+        })
+    } else {
+        return res.status(401).json({ message: 'Unauthorized' })
+    }
+}
+
+
+
+module.exports = {
+    configSecret
+}
