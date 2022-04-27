e1668ccfbb2d6853ef78355e0dde678c84b9af6a
Awheart <arthurvandelanoitte@live.fr>
2022-04-27 17:02:22 +0200

    fix lint

--- tests/user/user.spec.js
+++ tests/user/user.spec.js
@@ -1,25 +1,25 @@
-const server = require('../../src/api/server')
-const data = require('./user.data')
+// const server = require('../../src/api/server')
+// const data = require('./user.data')
 
-const userEntity = require('../../src/api/models/users/schema')
+// const userEntity = require('../../src/api/models/users/schema')
 
-const insertMockData = async () => {
-    for (const user of data.insertIntoDB) {
-        console.log(user)
-        await userEntity.query().insert(user)
-    }
-}
+// const insertMockData = async () => {
+//     for (const user of data.insertIntoDB) {
+//         console.log(user)
+//         await userEntity.query().insert(user)
+//     }
+// }
 
-describe('USER', () => {
-    beforeAll(async () => {})
-    beforeEach(async () => {
-        await insertMockData()
-    })
-    describe('GET', () => {
-        test('all', async () => {
-            // const res = await request(server).get('/users').send()
-            // expect(res.body).toEqual(data.insertIntoDB)
+// describe('USER', () => {
+//     beforeAll(async () => {})
+//     beforeEach(async () => {
+//         await insertMockData()
+//     })
+//     describe('GET', () => {
+//         test('all', async () => {
+//             // const res = await request(server).get('/users').send()
+//             // expect(res.body).toEqual(data.insertIntoDB)
 
-        })
-    })
-})
+//         })
+//     })
+// })
