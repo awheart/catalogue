223d6ee6be36937650ecd489eb11daaa6e12dedd
Awheart <arthurvandelanoitte@live.fr>
2022-05-16 22:22:58 +0200

    user tests

--- tests/user/user.data.js
+++ tests/user/user.data.js
@@ -1,17 +1,19 @@
 exports.insertIntoDB = [
     {
         username: 'user1',
-        id: 1,
+        // id: 1,
         email: 'user1@example.com',
         role: 'user',
+        birth_date: '1989-05-13',
         icone: 'icone-user1',
         password: 'password',
     },
     {
         username: 'user2',
-        id: 2,
+        // id: 2,
         email: 'user2@example.com',
         role: 'user',
+        birth_date: '1996-05-13',
         icone: 'icone-user2',
         password: 'password',
     },
@@ -19,7 +21,8 @@ exports.insertIntoDB = [
         username: 'user3',
         email: 'user3@example.com',
         role: 'user',
-        id: 3,
+        birth_date: '1972-05-13',
+        // id: 3,
         icone: 'icone-user3',
         password: 'password',
     },
@@ -27,7 +30,8 @@ exports.insertIntoDB = [
         username: 'user4',
         email: 'user4@example.com',
         role: 'user',
-        id: 4,
+        birth_date: '1986-05-13',
+        // id: 4,
         icone: 'icone-user4',
         password: 'password',
     },
@@ -35,7 +39,8 @@ exports.insertIntoDB = [
         username: 'user5',
         email: 'user5@example.com',
         role: 'user',
-        id: 5,
+        birth_date: '2000-05-13',
+        // id: 5,
         icone: 'icone-user5',
         password: 'password',
     }
@@ -48,7 +53,7 @@ exports.users = {
             inputs: { url: '/api/users' },
             expects: {
                 all: [{
-                    "birth_date": null,
+                    "birth_date": expect.any(String),
                     "created_at": expect.any(String),
                     "description": null,
                     "email": "user1@example.com",
@@ -60,7 +65,7 @@ exports.users = {
                     "username": "user1",
                 },
                 {
-                    "birth_date": null,
+                    "birth_date": expect.any(String),
                     "created_at": expect.any(String),
                     "description": null,
                     "email": "user2@example.com",
@@ -72,7 +77,7 @@ exports.users = {
                     "username": "user2",
                 },
                 {
-                    "birth_date": null,
+                    "birth_date": expect.any(String),
                     "created_at": expect.any(String),
                     "description": null,
                     "email": "user3@example.com",
@@ -84,7 +89,7 @@ exports.users = {
                     "username": "user3",
                 },
                 {
-                    "birth_date": null,
+                    "birth_date": expect.any(String),
                     "created_at": expect.any(String),
                     "description": null,
                     "email": "user4@example.com",
@@ -96,7 +101,7 @@ exports.users = {
                     "username": "user4",
                 },
                 {
-                    "birth_date": null,
+                    "birth_date": expect.any(String),
                     "created_at": expect.any(String),
                     "description": null,
                     "email": "user5@example.com",
@@ -107,6 +112,173 @@ exports.users = {
                     "updated_at": expect.any(String),
                     "username": "user5",
                 }]
+            }
+        },
+        by_id: {
+            mocks: {},
+            inputs: { url: '/api/users/1' },
+            expects: {
+                by_id: {
+                    "birth_date": expect.any(String),
+                    "created_at": expect.any(String),
+                    "description": null,
+                    "email": "user1@example.com",
+                    "icone": "icone-user1",
+                    "id": 1,
+                    "password": "password",
+                    "role": "user",
+                    "updated_at": expect.any(String),
+                    "username": "user1"
+                }
+            }
+        },
+        by_filter: {
+            mocks: {},
+            inputs: {
+                url: '/api/users?email=user1@example.com'
+            },
+            expects: {
+                by_filter: [{
+                    birth_date: expect.any(String), 
+                    created_at: expect.any(String), 
+                    description: null, 
+                    email: "user1@example.com",
+                    icone: "icone-user1", 
+                    id: 1, 
+                    password: "password", 
+                    role: "user", 
+                    updated_at: expect.any(String), 
+                    username: "user1"
+                }]
+            }
+        },
+        invalid_id: {
+            mocks: {},
+            inputs: {
+                url: '/api/users/-1'
+            },
+            expects: {
+                invalid_id: {
+                    message: "L'id de cet utilisateur est invalide."
+                }
+            }
+        }
+    },
+    POST: {
+        one: {
+            mock: {},
+            inputs: { url: '/api/users/register', body: { email: 'userpost@gmail.com', password: 'password', username: 'userpost', role: 'test' } },
+            expects: {
+                one: {
+                    email: 'userpost@gmail.com',
+                    created_at: expect.any(String),
+                    updated_at: expect.any(String),
+                    password: expect.any(String),
+                    username: 'userpost',
+                    role: 'user',
+                    id: 6
+                }
+            }
+        },
+        invalid_email: {
+            mock: {},
+            inputs: { url: '/api/users/register', body: { email: 'userpostàgmail.com', password: 'password', username: 'userpost2', role: 'test' } },
+            expects: {
+                invalid_email: {
+                    errors: {
+                        email: {
+                            location: "body",
+                            msg: "Veuillez entrer une adresse email valide",
+                            param: "email",
+                            value: "userpostàgmail.com",
+                        },
+                    }
+                }
+            }
+        },
+        username_existing: {
+            mock: {},
+            inputs: {
+                url: '/api/users/register',
+                body: {
+                    email: 'userexisting@gmail.com',
+                    password: 'password',
+                    username: 'user2',
+                    role: 'test'
+                }
+            },
+            expects: {
+                username_existing: {
+                    errors: {
+                        username: {
+                            location: "body",
+                            msg: "Ce nom d'utilisateur existe déjà",
+                            param: "username",
+                            value: "user2",
+                        },
+                    }
+                }
+            }
+        }
+    },
+    PATCH: {
+        update_user: {
+            mocks: {},
+            inputs: {
+                url: '/api/users/1',
+                body: {
+                    password: 'newpassword'
+                }
+            },
+            expects: {
+                update_user: {
+                    birth_date: expect.any(String),
+                    created_at: expect.any(String),
+                    description: null,
+                    email: "user1@example.com",
+                    icone: "icone-user1",
+                    id: 1,
+                    password: expect.any(String),
+                    role: "user",
+                    updated_at: expect.any(String),
+                    username: "user1",
+                }
+            }
+        },
+        update_failed: {
+            mocks: {},
+            inputs: {
+                url: '/api/users/-1',
+                body: {
+                    password: 'newpassword'
+                }
+            },
+            expects: {
+                update_failed: {
+                    message: 'Utilisateur introuvable'
+                }
+            }
+        }
+    },
+    DELETE: {
+        delete_user: {
+            mocks: {},
+            inputs: {
+                url: '/api/users/1'
+            },
+            expects: {
+                delete_user: 'L\'utilisateur a bien été supprimé'
+            }
+        },
+        delete_failed: {
+            mocks: {},
+            inputs: {
+                url: '/api/users/-1'
+            },
+            expects: {
+                delete_failed: {
+                    message: 'Utilisateur introuvable'
+                }
             }
         }
     }
--- tests/user/user.spec.js
+++ tests/user/user.spec.js
@@ -4,7 +4,6 @@ const data = require('./user.data')
 const { initializeDatabase, destroyDatabase, migrateUp, migrateDown, insertUsers } = require('../helpers/database')
 
 describe('USER', () => {
-
     beforeAll(async () => {
         await initializeDatabase()
         await migrateUp()
@@ -18,16 +17,110 @@ describe('USER', () => {
     describe('GET', () => {
         test('all', async () => {
             const { inputs, expects } = data.users.GET.all
-            const res = await request(server).get(inputs.url)
+            const res = await request(server)
+                .get(inputs.url)
 
+            console.log(res.error)
             expect(res.statusCode).toBe(200)
             expect(res.body).toEqual(expects.all)
+            console.log(res)
+        })
+        test('by id', async () => {
+            const { inputs, expects } = data.users.GET.by_id
+            const res = await request(server)
+                .get(inputs.url)
+
+            expect(res.statusCode).toBe(200)
+            expect(res.body).toEqual(expects.by_id)
+        })
+        test('by filter', async () => {
+            const { inputs, expects } = data.users.GET.by_filter
+            const res = await request(server)
+                .get(inputs.url)
+
+            expect(res.statusCode).toBe(200)
+            expect(res.body).toEqual(expects.by_filter)
+        })
+        test('invalid id', async () => {
+            const { inputs, expects } = data.users.GET.invalid_id
+            const res = await request(server)
+                .get(inputs.url)
+
+            expect(res.statusCode).toBe(404)
+            expect(res.body).toEqual(expects.invalid_id)
         })
 
     })
     describe('POST', () => {
         test('one', async () => {
-            
+            const { inputs, expects } = data.users.POST.one
+            const res = await request(server)
+                .post(inputs.url)
+                .set('Accept', 'application/json')
+                .send(inputs.body)
+
+            expect(res.statusCode).toBe(200)
+            expect(res.body).toEqual(expects.one)
+        })
+        test('invalid email', async () => {
+            const { inputs, expects } = data.users.POST.invalid_email
+            const res = await request(server)
+                .post(inputs.url)
+                .set('Accept', 'application/json')
+                .send(inputs.body)
+
+            expect(res.statusCode).toBe(422)
+            expect(res.body).toEqual(expects.invalid_email)
+        })
+        test('username already in use', async () => {
+            const { inputs, expects } = data.users.POST.username_existing
+            const res = await request(server)
+                .post(inputs.url)
+                .set('Accept', 'application/json')
+                .send(inputs.body)
+
+            expect(res.statusCode).toBe(422)
+            expect(res.body).toEqual(expects.username_existing)
+        })
+    })
+    describe('PATCH', () => {
+        test('update user', async () => {
+            const { inputs, expects } = data.users.PATCH.update_user
+            const res = await request(server)
+                .patch(inputs.url)
+                .send(inputs.body)
+
+            expect(res.statusCode).toBe(200)
+            expect(res.body).toEqual(expects.update_user)
+        })
+        test('user not found', async () => {
+            const { inputs, expects } = data.users.PATCH.update_failed
+            const res = await request(server)
+                .patch(inputs.url)
+                .send(inputs.body)
+
+            expect(res.statusCode).toBe(404)
+            expect(res.body).toEqual(expects.update_failed)
+        })
+    })
+    describe('DELETE', () => {
+        test('delete user', async () => {
+            const { inputs, expects } = data.users.DELETE.delete_user
+            const res = await request(server)
+                .delete(inputs.url)
+                .send(inputs.body)
+
+            expect(res.statusCode).toBe(200)
+            expect(res.body).toEqual(expects.delete_user)
+        })
+        test('user not found', async () => {
+            const { inputs, expects } = data.users.DELETE.delete_failed
+            const res = await request(server)
+                .delete(inputs.url)
+                .send(inputs.body)
+
+            expect(res.statusCode).toBe(404)
+            expect(res.body).toEqual(expects.delete_failed)
         })
     })
 })
