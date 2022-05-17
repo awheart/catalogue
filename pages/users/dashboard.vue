033a14eaf77bff53676907cc235b898b3300ea2d
Awheart <arthurvandelanoitte@live.fr>
2022-05-17 23:14:36 +0200

    front changes to match back end

--- pages/users/dashboard.vue
+++ pages/users/dashboard.vue
@@ -6,7 +6,7 @@
                     <th>Nom d'utilisateur</th>
                     <th>email</th>
                     <th>rôle</th>
-                    <th>réseaux sociaux</th>
+                    <th>tranche d'âge</th>
                     <th>icône</th>
                     <th>id</th>
                 </tr>
@@ -16,10 +16,16 @@
                     <td>{{ user.username }}</td>
                     <td>{{ user.email }}</td>
                     <td>{{ user.role }}</td>
+                    <td>{{ user.age }}</td>
                     <td>{{ user.icone }}</td>
-                    <td>{{ user.users_id }}</td>
-                    <td><button @click="deleteUser(user)" class="btn btn-danger">delete</button></td>
-                    <td><button @click="$router.push(`/users/${user.users_id}/details`)" class="btn btn-primary mr-3">Détails</button></td>
+                    <td>{{ user.id }}</td>
+                    <td>
+                        <button @click="deleteUser(user)" class="btn btn-danger">delete</button>
+                    </td>
+                    <td>
+                        <button @click="$router.push(`/users/${user.id}/details`)"
+                            class="btn btn-primary mr-3">Détails</button>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -31,13 +37,14 @@ export default {
     name: "DashboardPage",
     data() {
         return {
-            Users: []
+            Users: [],
+            Recipes: []
         }
     },
     async mounted() {
         try {
-            const {data} = await this.$axios.get( '/api/users')
-            this.Users = data
+            const userData = await this.$axios.get(`/api/users`)
+            this.Users = userData.data
         } catch (err) {
             console.error(err)
         }
@@ -47,10 +54,10 @@ export default {
             if (this.$auth.user.email == user.email) return alert('cannot delete the account currently connected.')
             if (confirm('Are you sure you want to delete') === true) {
                 try {
-                    const res = await this.$axios.delete( `/api/users/${user.users_id}`)
+                    const res = await this.$axios.delete(`/api/users/${user.users_id}`)
                     if (res) {
-                        const {data} =  await this.$axios.get( 'api/users')
-                        if(user) this.Users = data
+                        const userData = await this.$axios.get('/api/users')
+                        if (user) this.Users = userData.data
                     }
                 } catch (err) {
                     console.error(err)
--- pages/users/login.vue
+++ pages/users/login.vue
@@ -3,20 +3,17 @@
     <h1>Login</h1>
     <hr>
 
-    <div class="alert alert-success"
-      v-if="$route.params.registered=='yes'">You have registered successfully</div>
+    <div class="alert alert-success" v-if="$route.params.registered == 'yes'">You have registered successfully</div>
 
     <div class="row">
       <div class="col-md-6">
-        <form action=""
-          method="post"
-          @submit.prevent="login()">
+        <form action="" method="post" @submit.prevent="login()">
+
+        <div class="text-danger" v-if="errors && errors.msg">{{ errors.msg }}</div>
 
           <div class="form-group">
             <label for="">Email</label>
-            <input type="text" class="form-control"
-              :class="{ 'is-invalid': errors && errors.email }"
-              v-model="email">
+            <input type="text" class="form-control" :class="{ 'is-invalid': errors && errors.email }" v-model="email">
             <div class="invalid-feedback" v-if="errors && errors.email">
               {{ errors.email.msg }}
             </div>
@@ -24,16 +21,14 @@
 
           <div class="form-group">
             <label for="">Password</label>
-            <input type="password" class="form-control"
-              :class="{ 'is-invalid': errors && errors.password }"
+            <input type="password" class="form-control" :class="{ 'is-invalid': errors && errors.password }"
               v-model="password">
             <div class="invalid-feedback" v-if="errors && errors.password">
               {{ errors.password.msg }}
             </div>
           </div>
 
-          <div class="alert alert-danger"
-            v-if="login_error">{{ login_error }}</div>
+          <div class="alert alert-danger" v-if="login_error">{{ login_error }}</div>
 
           <input type="submit" value="Login" class="btn btn-primary mr-3">
           <nuxt-link to="/" class="btn btn-secondary mr-3">Cancel</nuxt-link>
@@ -50,24 +45,31 @@ export default {
   middleware: 'auth',
   auth: 'guest',
   data() {
-    return{
-      errors:null,
-      login_error:null,
-      email:null,
-      password:null,
-      role:null,
-      status:false,
+    return {
+      errors: null,
+      login_error: null,
+      email: null,
+      password: null,
+      role: null,
+      status: false,
+      id: null
     }
   },
-  methods:{
-    async login(){
-      await this.$auth.loginWith('local', {
+  methods: {
+    async login() {
+      this.errors = null
+      try {
+        await this.$auth.loginWith('local', {
           data: {
             email: this.email,
-            password: this.password
+            password: this.password,
           }
         })
-        this.$toast.success('Vous êtes connecté.', { duration: 1000 })
+        this.$toast.success(`Vous êtes connecté.`, { duration: 1000 })
+      } catch (err) {
+        return this.errors = err.response.data.errors
+      }
+
     }
   }
 }
