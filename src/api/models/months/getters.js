d2ca35ffa69a7b6dfbb5de0b50d3832f5592252c
Awheart <arthurvandelanoitte@live.fr>
2022-05-25 11:07:42 +0200

    rename folder

--- src/api/models/months/getters.js
+++ src/api/models/months/getters.js
@@ -1,5 +1,5 @@
 const Month = require('./schema')
 
-exports.getAll = async filter => Month.query().select().where(filter ? filter : "")
+exports.getAll = async () => Month.query().select()
 exports.findById = async id => Month.query().select().findById(id)
 exports.findOne = async filter => Month.query().findOne(filter)
