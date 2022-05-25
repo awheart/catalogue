ebc5017780ccbdd1eceed536d7e453f5ae83e1c0
Awheart <arthurvandelanoitte@live.fr>
2022-05-25 11:08:15 +0200

    relation mapping on recipe + new getters

--- src/api/models/recipes/getters.js
+++ src/api/models/recipes/getters.js
@@ -1,5 +1,5 @@
 const Recipes = require('./schema')
 
-exports.getAll = async filter => Recipes.query().select().orderBy('created_at', 'desc').where(filter ? filter : '')
-exports.findById = async id => Recipes.query().select().findById(id)
-exports.findOne = async filter => Recipes.query().findOne(filter)
+exports.getAll = async filter => Recipes.query().select().orderBy('created_at', 'desc').where(filter ? filter : '').withGraphFetched('[price, author.[role]]')
+exports.findById = async id => Recipes.query().select().findById(id).withGraphFetched('[price, author]')
+exports.findOne = async filter => Recipes.query().findOne(filter).withGraphFetched('[price, author]')
--- src/api/models/recipes/schema.js
+++ src/api/models/recipes/schema.js
@@ -1,4 +1,11 @@
+const ListIngredient = require('../list_ingredient/schema')
 const model = require('../model')
+const Months = require('../months/schema')
+const RecipeComment = require('../recipe_comments/schema')
+const RecipePrice = require('../recipe_price/schema')
+const Step = require('../steps/schema')
+const Tags = require('../tags/schema')
+const Users = require('../users/schema')
 
 class Recipes extends model {
     static get tableName() {
@@ -7,13 +14,92 @@ class Recipes extends model {
     static get idColumn() {
         return 'id'
     }
+    static get relationMappings() {
+        return {
+            author: {
+                relation: model.BelongsToOneRelation,
+                modelClass: Users,
+                join: {
+                    from: 'recipes.author_id',
+                    to: 'users.id'
+                }
+            },
+            price: {
+                relation: model.BelongsToOneRelation,
+                modelClass: RecipePrice,
+                join: {
+                    from: 'recipes.price_id',
+                    to: 'recipe_price.id'
+                }
+            },
+            step: {
+                relation: model.HasManyRelation,
+                modelClass: Step,
+                join: {
+                    from: 'recipes.id',
+                    to: 'step.id'
+                }
+            },
+            tags: {
+                relation: model.ManyToManyRelation,
+                modelClass: Tags,
+                join: {
+                    from: 'recipes.id',
+                    through: {
+                        from: 'tag_recipe.recipe_id',
+                        to: 'tag_recipe.tag_id'
+                    },
+                    to: 'tag.id'
+                }
+            },
+            months_of_consumption: {
+                relation: model.ManyToManyRelation,
+                modelClass: Months,
+                join: {
+                    from: 'recipes.id',
+                    through: {
+                        from: 'month_of_consumption.recipe_id',
+                        to: 'month_of_consumption.month_id'
+                    },
+                    to: 'months.id'
+                }
+            },
+            list_ingredient: {
+                relation: model.HasManyRelation,
+                modelClass: ListIngredient,
+                join: {
+                    from: 'recipes.id',
+                    to: 'list_ingredient.id'
+                }
+            },
+            comments: {
+                relation: model.HasManyRelation,
+                modelClass: RecipeComment,
+                join: {
+                    from: 'recipes.id',
+                    to: 'recipe_comment.id'
+                }
+            },
+            likes: {
+                relation: model.ManyToManyRelation,
+                modelClass: Users,
+                join: {
+                    from: 'recipes.id',
+                    through: {
+                        from: 'like_recipe.recipe_id',
+                        to: 'like_recipe.month_id'
+                    },
+                    to: 'users.id'
+                }
+            }
+        }
+    }
     static get jsonSchema() {
         return {
             type: 'object',
             properties: {
                 id: { type: 'integer' },
                 title: { type: 'string' },
-                author: { type: 'string' },
                 description: { type: 'string' },
                 nbr_person: {
                     type: 'integer',
@@ -34,7 +120,8 @@ class Recipes extends model {
                     maximum: 10080,
                 },
                 image: { type: 'string' },
-                price: { type: 'string', enum: ['bon marché', 'accessible', 'je me fais plaisir'] }
+                price_id: { type: 'integer' },
+                author_id: { type: 'integer' }
             }
         }
     }
