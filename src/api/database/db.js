1d3f8f6085400b08e54a57b6bc874f7d48d8fcdf
Awheart <arthurvandelanoitte@live.fr>
2022-05-25 11:06:42 +0200

    init month table on server start

--- src/api/database/db.js
+++ src/api/database/db.js
@@ -1,9 +1,10 @@
-const { initRole, initPrice } = require('./index')
+const { initRole, initPrice, initMonths } = require('./index')
 
 exports.initTables = async () => {
     try {
         await initPrice()
         await initRole()
+        await initMonths()
     } catch (err) {
         console.log('errrrrr: ', err)
     }
