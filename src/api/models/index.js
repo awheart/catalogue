5d87d2b0e85251e4be31ec4f4c5793a1b409de11
Awheart <arthurvandelanoitte@live.fr>
2022-05-26 17:49:51 +0200

    update exports

--- src/api/models/index.js
+++ src/api/models/index.js
@@ -3,13 +3,12 @@ module.exports = {
     UserRole: require('./user_role'),
     RecipePrice: require('./recipe_price'),
     Recipes: require('./recipes'),
-    Recipe_comment: require('./recipe_comments'),
+    Recipe_comment: require('./comments'),
     Steps: require('./steps'),
     Tags: require('./tags'),
     Tag_recipe: require('./tag_recipe'),
     List_ingredients: require('./list_ingredient'),
     Like_recipe: require('./like_recipe'),
-    Refresh_token: require('./refresh_tokens'),
     Month_of_consumption: require('./months'),
     MonthOfConsumtionRecipe: require('./month_of_consumption')
 }
