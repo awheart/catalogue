d079be9a99546cefbfecc6ea7cd502fb9460eb67
Awheart <arthurvandelanoitte@live.fr>
2022-05-26 17:48:21 +0200

    remove previous comment

--- src/api/models/recipe_comments/mutations.js
+++
@@ -1,4 +0,0 @@
-const RecipeComment = require('./schema')
-
-exports.create = async comment => RecipeComment.query().insert(comment)
-exports.deleteById = async id => RecipeComment.query().deleteById(id)
