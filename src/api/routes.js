8f3e83cce3e05ac8bccd07d61538fe3125f8816c
Awheart <arthurvandelanoitte@live.fr>
2022-05-27 01:12:59 +0200

    add routes to api

--- src/api/routes.js
+++ src/api/routes.js
@@ -2,8 +2,13 @@ const router = require('express').Router({ strict: true })
 const users = require('./routes/user.routes')
 const recipes = require('./routes/recipe.routes')
 const comments = require('./routes/comment.routes')
+const steps = require('./routes/step.routes')
+const likes = require('./routes/like.routes')
 
 router.use('/api/users', users)
 router.use('/api/recipes', recipes)
 router.use('/api/comments', comments)
+router.use('/api/likes', likes)
+router.use('/api/steps', steps)
+
 module.exports = router
