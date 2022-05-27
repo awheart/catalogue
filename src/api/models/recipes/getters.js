7be03b61a4fee08c8cde08e5c8ef70c263d2136e
Awheart <arthurvandelanoitte@live.fr>
2022-05-27 20:10:55 +0200

    modifiers on recipe getter

--- src/api/models/recipes/getters.js
+++ src/api/models/recipes/getters.js
@@ -6,13 +6,22 @@ exports.getAll = async filter =>
         .select()
         .orderBy('created_at', 'desc')
         .where(filter)
-        .withGraphFetched('[price, author.[role], steps(inOrderStep), comments.[author], user_like, months, tags, list_ingredient(inOrderList)]')
+        .withGraphFetched('[price, author(selectDefaultAuthor).[role(selectDefaultRole)], steps(inOrderStep), comments(selectDefaultComment).[author(selectDefaultAuthor)], user_like(selectDefaultAuthor), months, tags, list_ingredient(inOrderList)]')
         .modifiers({
             inOrderStep(builder) {
-                builder.orderBy('step_order', 'asc')
+                builder.select('content', 'id').orderBy('step_order', 'asc')
             },
             inOrderList(builder) {
-                builder.orderBy('inlist_order', 'asc')
+                builder.select('content', 'id').orderBy('inlist_order', 'asc')
+            },
+            selectDefaultAuthor(builder) {
+                builder.select('users.id', 'users.username')
+            },
+            selectDefaultRole(builder) {
+                builder.select('role_name')
+            },
+            selectDefaultComment(builder) {
+                builder.select('content', 'id')
             }
         })
 exports.findById = async id => Recipes.query().select().findById(id).withGraphFetched('[price, author.[role], steps, comments.[author], user_like, months, tags]')
