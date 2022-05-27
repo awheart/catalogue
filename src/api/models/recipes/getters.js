7c1f759052a02448dcd36225534c6acf795dc23f
Awheart <arthurvandelanoitte@live.fr>
2022-05-27 20:16:58 +0200

    fix readability & add modifiers on all query

--- src/api/models/recipes/getters.js
+++ src/api/models/recipes/getters.js
@@ -1,28 +1,37 @@
 const Recipes = require('./schema')
+const graphFilter = '[price, author(selectDefaultAuthor).[role(selectDefaultRole)], steps(inOrderStep), comments(selectDefaultComment).[author(selectDefaultAuthor)], user_like(selectDefaultAuthor), months, tags, list_ingredient(inOrderList)]'
+const modifiers = {
+    inOrderStep(builder) {
+        builder.select('content', 'id').orderBy('step_order', 'asc')
+    },
+    inOrderList(builder) {
+        builder.select('content', 'id').orderBy('inlist_order', 'asc')
+    },
+    selectDefaultAuthor(builder) {
+        builder.select('users.id', 'users.username')
+    },
+    selectDefaultRole(builder) {
+        builder.select('role_name')
+    },
+    selectDefaultComment(builder) {
+        builder.select('content', 'id')
+    }
+}
+exports.getAll = async filter => Recipes.query()
+    .skipUndefined()
+    .select()
+    .orderBy('created_at', 'desc')
+    .where(filter)
+    .withGraphFetched(graphFilter)
+    .modifiers(modifiers)
 
-exports.getAll = async filter =>
-    Recipes.query()
-        .skipUndefined()
-        .select()
-        .orderBy('created_at', 'desc')
-        .where(filter)
-        .withGraphFetched('[price, author(selectDefaultAuthor).[role(selectDefaultRole)], steps(inOrderStep), comments(selectDefaultComment).[author(selectDefaultAuthor)], user_like(selectDefaultAuthor), months, tags, list_ingredient(inOrderList)]')
-        .modifiers({
-            inOrderStep(builder) {
-                builder.select('content', 'id').orderBy('step_order', 'asc')
-            },
-            inOrderList(builder) {
-                builder.select('content', 'id').orderBy('inlist_order', 'asc')
-            },
-            selectDefaultAuthor(builder) {
-                builder.select('users.id', 'users.username')
-            },
-            selectDefaultRole(builder) {
-                builder.select('role_name')
-            },
-            selectDefaultComment(builder) {
-                builder.select('content', 'id')
-            }
-        })
-exports.findById = async id => Recipes.query().select().findById(id).withGraphFetched('[price, author.[role], steps, comments.[author], user_like, months, tags]')
-exports.findOne = async filter => Recipes.query().findOne(filter).withGraphFetched('[price, author.[role], steps, comments.[author], user_like, months, tags]')
+exports.findById = async id => Recipes.query()
+    .select()
+    .findById(id)
+    .withGraphFetched(graphFilter)
+    .modifiers(modifiers)
+
+exports.findOne = async filter => Recipes.query()
+    .findOne(filter)
+    .withGraphFetched(graphFilter)
+    .modifiers(modifiers)
--- src/api/models/users/getters.js
+++ src/api/models/users/getters.js
@@ -1,5 +1,32 @@
 const Users = require('./schema')
+const graphFilter = '[role(selectDefaultRole), recipes(selectDefaultRecipes), comments(selectDefaultComment).[recipe(selectDefaultRecipes)], liked_recipes(selectDefaultRecipes)]'
+const modifiers = {
+    selectDefaultRecipes(builder) {
+        builder.select('recipes.id', 'recipes.title')
+    },
+    selectDefaultRole(builder) {
+        builder.select('role_name')
+    },
+    selectDefaultComment(builder) {
+        builder.select('content', 'id')
+    }
+}
 
-exports.getAll = async filter => Users.query().skipUndefined().select().where(filter).withGraphFetched('[role, recipes, comments, liked_recipes]')
-exports.findById = async id => Users.query().select().findById(id).withGraphFetched('[role]')
-exports.findOne = async filter => Users.query().skipUndefined().findOne(filter).withGraphFetched('[role]')
+exports.getAll = async filter => Users.query()
+    .skipUndefined()
+    .select()
+    .where(filter)
+    .withGraphFetched(graphFilter)
+    .modifiers(modifiers)
+
+exports.findById = async id => Users.query()
+    .select()
+    .findById(id)
+    .withGraphFetched(graphFilter)
+    .modifiers(modifiers)
+
+exports.findOne = async filter => Users.query()
+    .skipUndefined()
+    .findOne(filter)
+    .withGraphFetched(graphFilter)
+    .modifiers(modifiers)
