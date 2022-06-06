6d8ed8ce5664c6d3e65cd03dcacaf55474acac62
Awheart <arthurvandelanoitte@live.fr>
2022-06-06 16:05:43 +0200

    fix knexfile

--- knexfile.js
+++ knexfile.js
@@ -6,9 +6,9 @@ module.exports = {
   test: {
     client: 'pg',
     connection: {
-      host: process.env.POSTGRES_HOST,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
+      host: process.env.POSTGRES_HOST || 'localhost',
+      user: process.env.POSTGRES_USER || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || 'postgres',
       database: 'test_database',
       port: 5432
     },
@@ -24,10 +24,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      host: process.env.POSTGRES_HOST,
-      user: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
+      host: process.env.POSTGRES_HOST || 'localhost',
+      user: process.env.POSTGRES_USER || 'postgres',
+      password: process.env.POSTGRES_PASSWORD || 'postgres',
+      database: process.env.POSTGRES_DB || 'dev_database',
       port: 5432
     },
     pool: {
