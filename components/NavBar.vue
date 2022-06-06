d850b7fb18cfea492cfbd932efae496d6dfac992
Awheart <arthurvandelanoitte@live.fr>
2022-06-06 17:07:09 +0200

    nav bar responsive

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,95 +1,164 @@
 <template>
     <div>
         <nav class="custom-navbar">
-            <ul class="custom-nav nav-logo">
-                <li class="navigation-item">
-                    <nuxt-link to="/"><img
-                            src="https://res.cloudinary.com/catalogue-recipe/image/upload/c_scale,w_200/v1653911048/logo_transparent_background_rtr96w.png"
-                            alt="logo vos recettes faciles">
-                    </nuxt-link>
-                </li>
-            </ul>
-            <ul class="custom-nav nav-page">
-                <li class="navigation-item">
-                    <nuxt-link class="normal-link" to="/" alt="retour à l'accueil"> Accueil
-                    </nuxt-link>
-                </li>
-                <li class="navigation-item">
-                    <nuxt-link class="normal-link" to="/recipes" alt="toutes les recettes">
-                        Recettes </nuxt-link>
-                </li>
-                <li class="navigation-item">
-                    <nuxt-link class="normal-link" to="/recipes/add" alt="ajouter une recette">
-                        Ajouter une recette
-                    </nuxt-link>
-                </li>
-                <li class="navigation-item">
-                    <nuxt-link class="normal-link" to="/recipes/recetomatic" alt="le recetomatic">
-                        Recet'Omatic
-                    </nuxt-link>
-                </li>
-            </ul>
-            <ul class="custom-nav nav-connect" v-if="!$auth.loggedIn">
-                <li class="navigation-item">
-                    <nuxt-link class="icon-link" to="/users/login" alt="se connecter">
-                        <b-icon icon='person-circle' width="48px" height="48px"></b-icon>
-                    </nuxt-link>
-                </li>
-
-            </ul>
-            <ul class="custom-nav nav-user" v-if="$auth.loggedIn">
-                <li class="navigation-icon" v-if="$auth.user.role.role_name == 'admin'">
-                    <nuxt-link class="icon-link" to="/users/dashboard" alt="panneau d'administration">
-                        <b-icon icon='gear-fill' width="48px" height="48px"></b-icon>
-                    </nuxt-link>
-                </li>
-                <li class="navigation-icon">
-                    <nuxt-link class="icon-link" to="/users/my-account" alt="mon compte">
-                        <b-icon icon='person-circle' width="48px" height="48px"></b-icon>
-                    </nuxt-link>
-                </li>
-                <li class="navigation-icon">
-                    <nuxt-link class="icon-link" to="/users/logout" alt="se déconnecter">
-                        <b-icon icon='box-arrow-right' width="48px" height="48px"></b-icon>
-                    </nuxt-link>
-                </li>
-            </ul>
+            <div class="burger" @click="show = !show">
+                <b-icon icon="list" width="48px" height="48px"></b-icon>
+            </div>
+            <nuxt-link to="/"><img
+                    src="https://res.cloudinary.com/catalogue-recipe/image/upload/c_scale,w_200/v1653911048/logo_transparent_background.png"
+                    alt="logo vos recettes faciles">
+            </nuxt-link>
+            <nuxt-link class="normal-link" to="/" alt="retour à l'accueil"> Accueil
+            </nuxt-link>
+            <nuxt-link class="normal-link" to="/recipes" alt="toutes les recettes">
+                Recettes </nuxt-link>
+            <nuxt-link class="normal-link" to="/recipes/add" alt="ajouter une recette">
+                Ajouter une recette
+            </nuxt-link>
+            <nuxt-link class="normal-link" to="/recipes/recetomatic" alt="le recetomatic">
+                Recet'Omatic
+            </nuxt-link>
+            <nuxt-link v-if="!$auth.loggedIn" class="icon-link" to="/users/login" alt="se connecter">
+                <b-icon icon='person-circle' width="48px" height="48px"></b-icon>
+            </nuxt-link>
+            <nuxt-link class="icon-link" v-if="$auth.loggedIn && $auth.user.role.role_name == 'admin'"
+                to="/users/dashboard" alt="panneau d'administration">
+                <b-icon icon='gear-fill' width="48px" height="48px"></b-icon>
+            </nuxt-link>
+            <nuxt-link class="icon-link" v-if="$auth.loggedIn" to="/users/my-account" alt="mon compte">
+                <b-icon icon='person-circle' width="48px" height="48px"></b-icon>
+            </nuxt-link>
+            <nuxt-link class="icon-link" v-if="$auth.loggedIn" to="/users/logout" alt="se déconnecter">
+                <b-icon icon='box-arrow-right' width="48px" height="48px"></b-icon>
+            </nuxt-link>
         </nav>
+        <transition name="hiddenTransition">
+            <div class="hidden" v-if="show">
+                <ul class="hidden-list">
+                    <li>
+                        <nuxt-link class="normal-link" to="/" alt="retour à l'accueil"> Accueil
+                        </nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link class="normal-link" to="/recipes" alt="toutes les recettes">
+                            Recettes </nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link class="normal-link" to="/recipes/add" alt="ajouter une recette">
+                            Ajouter une recette
+                        </nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link class="normal-link" to="/recipes/recetomatic" alt="le recetomatic">
+                            Recet'Omatic
+                        </nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link v-if="!$auth.loggedIn" class="icon-link" to="/users/login" alt="se connecter">
+                            <b-icon icon='person-circle' width="48px" height="48px"></b-icon>
+                        </nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link class="icon-link" v-if="$auth.loggedIn && $auth.user.role.role_name == 'admin'"
+                            to="/users/dashboard" alt="panneau d'administration">
+                            <b-icon icon='gear-fill' width="48px" height="48px"></b-icon>
+                        </nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link class="icon-link" v-if="$auth.loggedIn" to="/users/my-account" alt="mon compte">
+                            <b-icon icon='person-circle' width="48px" height="48px"></b-icon>
+                        </nuxt-link>
+                    </li>
+                    <li>
+                        <nuxt-link class="icon-link" v-if="$auth.loggedIn" to="/users/logout" alt="se déconnecter">
+                            <b-icon icon='box-arrow-right' width="48px" height="48px"></b-icon>
+                        </nuxt-link>
+                    </li>
+                </ul>
+            </div>
+        </transition>
     </div>
 </template>
 
+<script>
+export default {
+    data() {
+        return {
+            show: false
+        }
+    }
+}
+</script>
+
 <style scoped>
+.hidden-list {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    top: 15vh;
+}
+
+.hidden-list li {
+    list-style: none;
+    padding: 0.5em 0.5em;
+}
+
+.hidden-list li a {
+    display: block;
+    float: left;
+}
+
+.hidden {
+    position: absolute;
+    display: none;
+    top: 10vh;
+    width: 100%;
+    z-index: 100;
+    height: 85vh;
+    background-color: rgb(255, 255, 255);
+    border: dashed #FF5700;
+    border-radius: 0 0 0.5em 0.5em;
+    overflow: hidden;
+}
+
+.hiddenTransition-enter-active,
+.hiddenTransition-leave-active {
+    transition: height 0.5s
+}
+
+.hiddenTransition-enter,
+.hiddenTransition-leave-to {
+    height: 0vh;
+}
+
+.burger {
+    display: none;
+}
+
+@media screen and (max-width: 800px) {
+    .custom-navbar.responsive {
+        position: relative;
+    }
+
+    .custom-navbar.responsive .icon {
+        position: absolute;
+        right: 0;
+        top: 0;
+    }
+
+    .custom-navbar.responsive a {
+        float: none;
+        display: block;
+        text-align: left;
+    }
+}
+
 .nuxt-link-exact-active {
     color: #FF5700;
 }
 
 .custom-navbar ul li {
     list-style: none;
-}
-
-.custom-nav {
-    display: flex;
-    align-items: flex-start;
-    flex-wrap: wrap;
-    padding-left: 0;
-    margin-bottom: 0;
-    list-style: none;
-}
-
-.nav-page,
-.nav-logo,
-.nav-connect,
-.nav-user {
-    position: absolute;
-}
-
-.nav-logo {
-    left: 5%;
-}
-
-.nav-connect,
-.nav-user {
-    right: 5%;
 }
 
 .custom-navbar {
@@ -111,24 +180,12 @@
     list-style: none;
 }
 
-.navigation-item {
-    margin-right: 5vw;
-}
-
-a {
-    padding: 0.5rem 1rem;
-}
-
 a:hover {
     text-decoration: none;
 }
 
-.nav-page {
-    width: max-content;
-}
-
-.last-nav-item {
-    margin-right: 0;
+.icon {
+    display: block;
 }
 
 a {
@@ -165,5 +222,24 @@ a:hover {
 
 a:active {
     color: #FF5700;
+}
+
+@media screen and (max-width: 800px) {
+    .custom-navbar a:not(:nth-child(-n + 2)) {
+        display: none;
+    }
+
+    .custom-navbar a.icon {
+        float: right;
+        display: block;
+    }
+
+    .burger {
+        display: block;
+    }
+
+    .hidden {
+        display: block;
+    }
 }
 </style>
