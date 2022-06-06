a7e87034194d34378afbfdce83c4cde970a3ea46
Awheart <arthurvandelanoitte@live.fr>
2022-06-06 15:32:13 +0200

    secret jwt

--- src/api/controllers/user.controller.js
+++ src/api/controllers/user.controller.js
@@ -75,7 +75,7 @@ module.exports.login = [
                     name: user.name,
                     role: user.role
                 },
-                token: jwt.sign({ _id: user._id, email: user.email, username: user.username, role: user.role }, process.env.JWT_TOKEN, { expiresIn: '15m' })
+                token: jwt.sign({ _id: user._id, email: user.email, username: user.username, role: user.role }, process.env.JWT_TOKEN || 'secret', { expiresIn: '15m' })
             })
         } else {
             return res.status(500).json({ message: Error_Messages.invalid_credentials })
