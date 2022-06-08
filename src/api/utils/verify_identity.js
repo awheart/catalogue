e3f9f405922e72f920509561415f997cdc3a6153
awheart <arthurvandelanoitte@live.fr>
2022-06-08 19:29:01 +0200

    config jwt token

--- src/api/utils/verify_identity.js
+++ src/api/utils/verify_identity.js
@@ -1,11 +1,12 @@
 const jwt = require('jsonwebtoken')
+const config = require('../config/config')
 
 // check if user is connected
 const isAuthenticated = async (req, res, next) => {
     const token = req.headers.authorization
     if (token) {
         // check if token is valid and verifies secret
-        jwt.verify(token.replace(/^Bearer\s/, ''), process.env.JWT_TOKEN, (err) => {
+        jwt.verify(token.replace(/^Bearer\s/, ''), config.jwt.token, (err) => {
             return (err ? res.status(401).json({ message: 'Unauthorized' }) : next())
         })
     } else {
@@ -17,7 +18,7 @@ const isAdmin = async (req, res, next) => {
     const token = req.headers.authorization
     if (token) {
         // check if token is valid and verifies secret
-        jwt.verify(token.replace(/^Bearer\s/, ''), process.env.JWT_TOKEN, (err, decoded) => {
+        jwt.verify(token.replace(/^Bearer\s/, ''), config.jwt.token, (err, decoded) => {
             if (err) {
                 return res.status(401).json({ message: 'Unauthorized' })
             } else if(decoded.role_id !== 2) return res.status(401).json({ message: 'Unauthorized' })
